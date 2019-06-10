7d7106693aaee5e6dd7ca56f7549aa3ceffae25f
Nathan Gilbert <nathan.gilbert@gmail.com>
2019-06-09 19:05:55 -0600

    Reindent file for 2 spaces

--- src/main/kotlin/SystemConfig.kt
+++ src/main/kotlin/SystemConfig.kt
@@ -5,13 +5,13 @@ import koref.utils.YAMLParser
 data class SystemConfigDto(val baseDataDir: String)
 
 class SystemConfig(private val settingsFile:String = "settings.yml") {
-    var isInitialized:Boolean = false
+  var isInitialized:Boolean = false
 
-    init {
-        if (!settingsFile.isEmpty()) {
-            //todo read in the file and set configuration values...
-            val config: SystemConfigDto = YAMLParser.parseDto("settings.yml", SystemConfigDto::class)
-            isInitialized = true
-        }
+  init {
+    if (!settingsFile.isEmpty()) {
+      //todo read in the file and set configuration values...
+      val config: SystemConfigDto = YAMLParser.parseDto("settings.yml", SystemConfigDto::class)
+      isInitialized = true
     }
+  }
 }
