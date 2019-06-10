eddf92cd33fbed50ac167c61e792f5aed1b7a93b
Nathan Gilbert <nathan.gilbert@gmail.com>
2019-06-09 19:08:00 -0600

    fix merge

--- src/main/kotlin/SystemConfig.kt
+++ src/main/kotlin/SystemConfig.kt
@@ -4,15 +4,6 @@ import koref.utils.YamlParser
 
 data class SystemConfigDto(val baseDataDir: String)
 
-<<<<<<< HEAD
-class SystemConfig(private val settingsFile:String = "settings.yml") {
-  var isInitialized:Boolean = false
-
-  init {
-    if (!settingsFile.isEmpty()) {
-      //todo read in the file and set configuration values...
-      val config: SystemConfigDto = YAMLParser.parseDto("settings.yml", SystemConfigDto::class)
-=======
 class SystemConfig(private val settingsFile: String = "settings.yml") {
   var isInitialized: Boolean = false
 
@@ -21,7 +12,6 @@ class SystemConfig(private val settingsFile: String = "settings.yml") {
 
       //todo read in the file and set configuration values...
       val config: SystemConfigDto = YamlParser.parseDto("settings.yml", SystemConfigDto::class)
->>>>>>> 37943631c79107447e665583b5ed317b12579897
       isInitialized = true
     }
   }
